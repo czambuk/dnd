e6d408dc5624146f1ada62c7ee8149588f3a4450
czambuk <tegruszecki@gmail.com>
2020-03-21 18:03:35 +0100

    'v02.1'

--- landing_page/static/landing_page/main_site.js
+++ landing_page/static/landing_page/main_site.js
@@ -1,16 +1,25 @@
+// General variables
 var $mainMenuItem = $(".mainMenuItem");
 
+// Unlimited main menu events after mouseover
 $mainMenuItem.on('mouseenter', function () {
     $(this).animate({
         width: 265,
         height: 265,
     }, 250);
     $(this).find('img').addClass("mainMenuShadow");
+    var $descriptionDiv = $("<div class='mainMenuDesc'>DIVDIVDIV</div>");
+//   Description div addition
+//    $(this).before($descriptionDiv);
 });
+
+// Unlimited main menu events after mouseleave
 $mainMenuItem.on("mouseleave", function () {
     $(this).animate({
         width: 250,
         height: 250,
     }, 250);
     $(this).find('img').removeClass("mainMenuShadow");
+//    Description div removal
+//    $(this).siblings('.mainMenuDesc').remove();
 });
