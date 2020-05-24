18e3a20ead84b616517e0e1ebf19e47c69884776
czambuk <tegruszecki@gmail.com>
2020-05-24 19:05:44 +0200

    heroku changes 05

--- dnd/settings.py
+++ dnd/settings.py
@@ -132,7 +132,10 @@ import django_heroku
 django_heroku.settings(locals())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# if os.environ.get("DEBUG") == 'TRUE':
-#     DEBUG = True
-# elif os.environ.get("DEBUG") == 'FALSE':
-DEBUG = False
+if os.environ.get("DEBUG") == 'TRUE':
+    DEBUG = True
+elif os.environ.get("DEBUG") == 'FALSE':
+    DEBUG = False
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
